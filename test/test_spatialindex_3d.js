0edda9192c32e702a5f1b233f3e66ae0e1c42b14
Davis Marques <dmarques@unimelb.edu.au>
2016-02-15 15:18:51 +1100

    Spatial index WIP

---
+++ test/test_spatialindex_3d.js
@@ -0,0 +1,36 @@
+'use strict';
+
+describe('spatial index 3D', function () {
+
+  describe('entity contained by one bucket', function () {
+    it('should add entity to correct bucket', function (done) {
+      done(false);
+    });
+    it('should retrieve the entity from the bucket', function (done) {
+      done(false);
+    });
+  });
+
+  describe('entity spanning two buckets', function () {
+    it('should add entity to correct buckets', function (done) {
+      done(false);
+    });
+    it('should retrieve the entity from each bucket', function (done) {
+      done(false);
+    });
+  });
+
+  describe('entity spanning four buckets', function () {
+    it('should add entity to correct buckets', function (done) {
+      done(false);
+    });
+    it('should retrieve the entity from each bucket', function (done) {
+      done(false);
+    });
+  });
+
+  it('should clear the index', function (done) {
+    done(false);
+  });
+
+});
