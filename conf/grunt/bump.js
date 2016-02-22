bdddaf925a30693bf601a5ced7befac7d7db24b9
Davis Marques <dmarques@unimelb.edu.au>
2016-02-22 11:21:28 +1100

    Update bump task. Add distribution.

--- conf/grunt/bump.js
+++ conf/grunt/bump.js
@@ -4,13 +4,13 @@ module.exports = {
   options: {
     files: ['bower.json', 'package.json'],
     updateConfigs: [],
-    commit: false,
+    commit: true,
     commitMessage: 'Release v%VERSION%',
     commitFiles: ['bower.json', 'package.json'],
-    createTag: false,
+    createTag: true,
     tagName: 'v%VERSION%',
     tagMessage: 'Version %VERSION%',
-    push: false,
+    push: true,
     pushTo: 'origin',
     gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
   }
